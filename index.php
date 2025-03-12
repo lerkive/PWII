5e571b4c8f6787b7ff4ad920cb257c2952297965
lerkive <leticiaoliveirasilva898@gmail.com>
2025-03-12 11:18:30 -0300

    AulaUmParteDois

--- index.php
+++ index.php
@@ -2,7 +2,13 @@
 
 include 'header.php';
 
-echo'Olá';
+$pagina = $_GET['pagina'];
+
+switch ($pagina) {
+    case 'home': include 'view/home.php'; break;
+    case 'contatos': include 'view/contacts.php'; break;
+    default: include 'view/home.php'; break;
+}
 
 include 'footer.php';
 
